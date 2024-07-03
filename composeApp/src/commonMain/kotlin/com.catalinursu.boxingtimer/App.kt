cdf2965ae34e462f7793af3c2910bc8e31aaae1f
catalin_personal <catalin.ursu407@gmail.com>
2024-07-03 23:36:32 +0300

    remove unused packages

--- composeApp/src/commonMain/kotlin/com.catalinursu.boxingtimer/App.kt
+++ composeApp/src/commonMain/kotlin/com.catalinursu.boxingtimer/App.kt
@@ -1,16 +1,7 @@
 package com.catalinursu.boxingtimer
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.catalinursu.boxingtimer.ui.screen.home.MainScreen
-import com.catalinursu.boxingtimer.ui.screen.home.components.DurationPickerDialog
 import com.catalinursu.boxingtimer.ui.screen.home.components.RoundsPickerDialog
-import com.catalinursu.boxingtimer.ui.screen.home.components.TopBar
-import com.catalinursu.boxingtimer.ui.screen.settings.SettingsScreen
 import com.catalinursu.boxingtimer.ui.theme.AppTheme
 
 @Composable
--- composeApp/src/commonMain/kotlin/com.catalinursu.boxingtimer/ui/screen/settings/components/TopBar.kt
+++ composeApp/src/commonMain/kotlin/com.catalinursu.boxingtimer/ui/screen/settings/components/TopBar.kt
@@ -2,9 +2,6 @@ package com.catalinursu.boxingtimer.ui.screen.settings.components
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 
